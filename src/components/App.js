a0fd6a86a033358ab8352c03eccf806fd09d3820
abdulrawoofali <43907143+abdulrawoofali@users.noreply.github.com>
2020-11-10 18:49:36 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
+		this.state = { visible: false };
+	}
+
+	handleClick = () => {
+		console.log("handling click.....");
+		this.setState({ visible: !this.state.visible });
 	};
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+	render() {
+		return (
+			<div id="main">
+				{this.state.visible ? (
+					<p id="para">
+						Hello, I've learnt to use the full-stack evaluation tool. This makes
+						me so happy
+					</p>
+				) : null}
+				<button id="click" onClick={this.handleClick}>
+					show
+				</button>
+			</div>
+		);
+	}
 }
 
-
 export default App;
-
